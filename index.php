0e2505f18ec7850fc3cee181c815da2f8855ad69
fronomenal <94882375+fronomenal@users.noreply.github.com>
2022-07-11 15:21:24 +0100

    implemented and styled index body

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+    include_once("./components/head.php")
+?>
+<?php
+    include_once("./components/index-header.php")
+?>
+<main>
+    <div class="container-lg main">
+        <div class="row mt-4">
+            <div class="col-lg-3 col-md-6 col-sm-12 text-center mb-3">
+                <section class="card cus-card">
+                    <img class="cus-card-img" src="">
+                    <div class="card-body">
+                        <h4 class="card-title">placeholder</h3>
+                        <p class="card-text cus-card-text"></p>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+</main>
+<?php
+    include_once("./components/footer.php")
+?>
