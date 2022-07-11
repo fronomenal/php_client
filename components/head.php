b8f11ff1e180d9f5f7065c3f2609351b37459ea1
fronomenal <94882375+fronomenal@users.noreply.github.com>
2022-07-11 15:20:27 +0100

    project restructure

---
+++ components/head.php
@@ -0,0 +1,10 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet"/>
+    <link href="assets/css/style.css" rel="stylesheet"/>
+    <script src="vendor/twbs/bootstrap/dist/js/bootstrap.min.js" defer></script>
+    <title>Document</title>
+</head>
--- components/index-header.php
+++ components/index-header.php
@@ -1,21 +1,7 @@
-<?php
-
-?>
-
-
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet"/>
-    <script src="vendor/twbs/bootstrap/dist/js/bootstrap.min.js" defer></script>
-    <title>Document</title>
-</head>
 <body>
     <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
+    <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -30,10 +16,5 @@
         </li>
       </ul>
     </div>
-  </div>
-</nav>
+    </nav>
     </header>
-    <main></main>
-    <footer></footer>
-</body>
-</html>
